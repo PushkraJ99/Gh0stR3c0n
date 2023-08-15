949748c26eff41236210ffc6ba0fdee3c948d76c
PushkraJ <104222808+PushkraJ99@users.noreply.github.com>
2023-08-15 12:55:57 +0530

    Create requirement.sh
    Don't Need to type and install python Libraries

---
+++ requirement.sh
@@ -0,0 +1,8 @@
+pip install pysqlite
+pip3 install pysqlite
+pip install fastapi
+pip install "uvicorn[standard]"
+pip3 install fastapi
+pip3 install "uvicorn[standard]"
+pip install Jinja2
+pip3 install Jinja2
